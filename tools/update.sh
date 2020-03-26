1fabb072aa7171fc6eac5825a41f6b425fa11c20
ryanaltair <al_tair@live.com>
2020-03-26 19:12:50 +0800

    better update.sh

--- tools/update.sh
+++ tools/update.sh
@@ -3,6 +3,12 @@ host=user@remote
 port=22
 keypath=~/.ssh/id_rsa
 project=$(basename $PWD)
-ssh -i ${keypath} -p ${port} ${host} "mkdir ~/remote_build/" 
-rsync --delete -avhe "ssh -i ${keypath} -p ${port}" ./ ${host}:~/remote_build/${project}
-ssh -i ${keypath} -p ${port} ${host} "cd ~/remote_build/${project}/ && npm run docker"
+dir_prefixed=~/remote_build
+ssh -i ${keypath} -p ${port} ${host} "mkdir ${dir_prefixed}" 
+rsync --delete -avhe "ssh -i ${keypath} -p ${port}" ./ ${host}:${dir_prefixed}/${project}
+# please install node on your remote
+# if node installed, but still find node command not found
+# try edit .bashrc and comment out this line
+# [ -z "$PS1" ] && return
+ssh -i ${keypath} -p ${port} ${host} "cd ${dir_prefixed}/${project}/ && npm run docker"
+
